5b4e418187b2d752d17f3cfa8e430907961828d0
Javohir <javohirfoziljonov2002@gmail.com>
2022-10-27 10:45:36 +0500

    commitni yomoni

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -5,28 +5,43 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "users")
 public class User {
-    private UUID id=UUID.randomUUID();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    private String chatId;
-    private String userState;
-    private String firstName;
-    private String lastName;
-    private String fileId;
-    private String caption;
-    private boolean photo=false;
+    public String chatId;
+    public int  STATS_FOR_PSYCHOLOGY;
+    public int  STATS_FOR_WORK_OR_HOME;
+    public int  STATS_FOR_GOVERNMENT;
+    public int  STATS_FOR_RIGHTS;
+    public String questionForPsychology;
+    public String questionForGovernment;
+    public String questionForRight;
+    public String userState;
+    public String firstName;
+    public String lastName;
+    public String fileId;
+    public String caption;
+    public boolean photo=false;
 
-    public User(String id) {
-        this.chatId = id;
-    }
-
-    public User(String id, String userState) {
-        this.chatId = id;
+    public User(String chatId, int STATS_FOR_PSYCHOLOGY, int STATS_FOR_WORK_OR_HOME, int STATS_FOR_GOVERNMENT, int STATS_FOR_RIGHTS, String questionForPsychology, String questionForGovernment, String questionForRight,String fileId, String userState) {
+        this.chatId = chatId;
+        this.STATS_FOR_PSYCHOLOGY = STATS_FOR_PSYCHOLOGY;
+        this.STATS_FOR_WORK_OR_HOME = STATS_FOR_WORK_OR_HOME;
+        this.STATS_FOR_GOVERNMENT = STATS_FOR_GOVERNMENT;
+        this.STATS_FOR_RIGHTS = STATS_FOR_RIGHTS;
+        this.questionForPsychology = questionForPsychology;
+        this.questionForGovernment = questionForGovernment;
+        this.questionForRight = questionForRight;
+        this.fileId = fileId;
         this.userState = userState;
     }
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,59 +1,54 @@
 package com.example.service;
-import com.example.database.Database;
 import com.example.entity.User;
 import com.example.feign.TelegramFeign;
 import com.example.payload.SendDocumentOwn;
 import com.example.payload.SendPhotoOwn;
 import com.example.payload.enums.UserStateNames;
+import com.example.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.*;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    Integer STATS_FOR_PSYCHOLOGY=0;
-    Integer STATS_FOR_WORK_OR_HOME=0;
-    Integer STATS_FOR_GOVERNMENT=0;
-    Integer STATS_FOR_RIGHTS=0;
-
     private  String text=null;
 
-    private final Queue<User> userForContractOrHome=new LinkedList<>();
-
     private final TelegramFeign telegramFeign;
-    private final Queue<String> queueForPsychology=new LinkedList<>();
-    private final Queue<String> queueForGovernment=new LinkedList<>();
-
-    private final Queue<String> queueForRights=new LinkedList<>();
     private final ReplyMarkup replyMarkup;
-    public  User getOrCreateUser(String chatId) {
-            for (User user : Database.users) {
-                if (user.getChatId().equals(chatId)) {
-                    return user;
-                }
-            }
-            User user = new User(chatId, UserStateNames.START.name());
-            Database.users.add(user);
+    private final UserRepo userRepo;
+
+    public User getUserFromUpdate(Update update){
+        if (update.hasMessage()){
+            org.telegram.telegrambots.meta.api.objects.User userFromUpdate = update.getMessage().getFrom();
+            Long id = userFromUpdate.getId();
+            Optional<User> optionalUser = userRepo.findById(id);
+            User user;
+            user = optionalUser.orElseGet(() -> new User(
+                    userFromUpdate.getId().toString(),
+                    1,
+                    1,
+                    1,
+                    1,
+                    " ",
+                    " ",
+                    " ",
+                    " ",
+                    UserStateNames.START.name()));
+            user=userRepo.save(user);
             return user;
-    }
-    public  User getUserFromUpdate(Update update) {
-        try {
-            if (update.hasMessage()) {
-                return getOrCreateUser(update.getMessage().getChatId().toString());
-            } else if (update.hasCallbackQuery()) {
-                return getOrCreateUser(update.getCallbackQuery().getMessage().getChatId().toString());
-            }
-            return new User();
         }
-        catch (Exception e){
-            User user = new User(update.getMessage().getChatId().toString(), UserStateNames.START.name());
-            Database.users.add(user);
+        else {
+            Long id = update.getCallbackQuery().getFrom().getId();
+            Optional<User> optionalUser = userRepo.findById(id);
+            User user=new User();
+            if (optionalUser.isPresent()) {
+                user=optionalUser.get();
+            }
             return user;
         }
     }
@@ -65,6 +60,7 @@ public class UserService {
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("Qanday muammoyingiz bor ? ");
             user.setUserState(UserStateNames.START.name());
+            user=userRepo.save(user);
             sendMessage.setReplyMarkup(replyMarkup.markup(user));
             telegramFeign.sendMessageToUser(sendMessage);
         }
@@ -76,10 +72,9 @@ public class UserService {
     public void stats(Update update){
         try {
             User user = getUserFromUpdate(update);
-            user.setUserState(UserStateNames.START.name());
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
-            sendMessage.setText("Bot foydalanuvchilari soni : " + Database.users.size());
+            sendMessage.setText("Bot foydalanuvchilari soni : " + userRepo.findAll().size());
             telegramFeign.sendMessageToUser(sendMessage);
         }
         catch (Exception e){
@@ -94,6 +89,7 @@ public class UserService {
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("Qay biriga muhtojsiz ? ");
+            user=userRepo.save(user);
             sendMessage.setReplyMarkup(replyMarkup.markup(user));
             telegramFeign.sendMessageToUser(sendMessage);
         }catch (Exception e){
@@ -109,6 +105,7 @@ public class UserService {
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("Savolingizni yozib qoldiring");
             user.setUserState(UserStateNames.QUESTION.name());
+            userRepo.save(user);
             telegramFeign.sendMessageToUser(sendMessage);
         }catch (Exception e){
             System.out.println(e.getMessage());
@@ -117,11 +114,11 @@ public class UserService {
 
     public void questionsForPsychology(Update update) {
         try {
-            STATS_FOR_PSYCHOLOGY++;
             String question = update.getMessage().getText();
-            queueForPsychology.add(question);
             User user = getUserFromUpdate(update);
             user.setUserState(UserStateNames.START.name());
+            user.setQuestionForPsychology(question);
+            userRepo.save(user);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("Savolga javobini 24 soat ichida https://t.me/leadergirls_umidnuri dan topasiz.\n" +
@@ -152,16 +149,21 @@ public class UserService {
             user.setUserState(UserStateNames.START.name());
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
-            int size = queueForPsychology.size();
-            if (!queueForPsychology.isEmpty()) {
-                for (int i = 0; i < size; i++) {
-                    questions = ((i + 1) + "." + queueForPsychology.poll() + "\n");
-                    sendMessage.setText(questions);
+            int i=0;
+            if (!userRepo.findAll().isEmpty()) {
+                for (User user1 : userRepo.findAll()) {
+                    if (!user1.getQuestionForPsychology().equals(" ")) {
+                        questions = ((++i) + "." + user1.getQuestionForPsychology() + "\n");
+                        sendMessage.setText(questions);
+                        user1.setQuestionForPsychology(" ");
+                        userRepo.save(user1);
+                        telegramFeign.sendMessageToUser(sendMessage);
+                    }
+                }
+                if(i==0) {
+                    sendMessage.setText("Qabul qilingan savollar yo'q");
                     telegramFeign.sendMessageToUser(sendMessage);
                 }
-            } else {
-                sendMessage.setText("Qabul qilingan savollar yo'q");
-                telegramFeign.sendMessageToUser(sendMessage);
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
@@ -254,11 +256,12 @@ public class UserService {
 
     public void questionsForGovernment(Update update) {
         try {
-            STATS_FOR_GOVERNMENT++;
             String question = update.getMessage().getText();
-            queueForGovernment.add(question);
             User user = getUserFromUpdate(update);
+            user.setQuestionForGovernment(question);
+            user.setSTATS_FOR_GOVERNMENT(user.getSTATS_FOR_GOVERNMENT()+1);
             user.setUserState(UserStateNames.START.name());
+            userRepo.save(user);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("So'rov qabul qilindi . Qo’shimcha savollar bo’lsa murojaat uchun @NuriG2");
@@ -273,6 +276,7 @@ public class UserService {
         try {
             User user = getUserFromUpdate(update);
             user.setUserState(UserStateNames.PROBLEM_WITH_RIGHTS.name());
+            userRepo.save(user);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("Kim tomonidan qanday huquqbuzarlik sodir etildi?");
@@ -285,11 +289,12 @@ public class UserService {
 
     public void questionsForRights(Update update) {
         try {
-            STATS_FOR_RIGHTS++;
             String question = update.getMessage().getText();
-            queueForRights.add(question);
             User user = getUserFromUpdate(update);
+            user.setQuestionForRight(question);
             user.setUserState(UserStateNames.START.name());
+            user.setSTATS_FOR_RIGHTS(user.getSTATS_FOR_RIGHTS()+1);
+            userRepo.save(user);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("So'rov qabul qilindi . Qo’shimcha savollar bo’lsa murojaat uchun @NuriG2");
@@ -306,16 +311,21 @@ public class UserService {
             user.setUserState(UserStateNames.START.name());
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
-            int size = queueForGovernment.size();
-            if (!queueForGovernment.isEmpty()) {
-                for (int i = 0; i < size; i++) {
-                    questions = ((i + 1) + "." + queueForGovernment.poll() + "\n");
-                    sendMessage.setText(questions);
+            int i=0;
+            if (!userRepo.findAll().isEmpty()) {
+                for (User user1 : userRepo.findAll()) {
+                    if (!user1.getQuestionForGovernment().equals(" ")) {
+                        questions = ((++i) + "." + user1.getQuestionForGovernment() + "\n");
+                        sendMessage.setText(questions);
+                        user1.setQuestionForGovernment(" ");
+                        userRepo.save(user1);
+                        telegramFeign.sendMessageToUser(sendMessage);
+                    }
+                }
+                if(i==0) {
+                    sendMessage.setText("Qabul qilingan savollar yo'q");
                     telegramFeign.sendMessageToUser(sendMessage);
                 }
-            } else {
-                sendMessage.setText("Qabul qilingan savollar yo'q");
-                telegramFeign.sendMessageToUser(sendMessage);
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
@@ -329,16 +339,21 @@ public class UserService {
             user.setUserState(UserStateNames.START.name());
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
-            int size = queueForRights.size();
-            if (!queueForRights.isEmpty()) {
-                for (int i = 0; i < size; i++) {
-                    questions = ((i + 1) + "." + queueForRights.poll() + "\n");
-                    sendMessage.setText(questions);
+            int i=0;
+            if (!userRepo.findAll().isEmpty()) {
+                for (User user1 : userRepo.findAll()) {
+                    if (!user1.getQuestionForRight().equals(" ")) {
+                        questions = ((++i) + "." + user1.getQuestionForRight() + "\n");
+                        sendMessage.setText(questions);
+                        user1.setQuestionForRight(" ");
+                        userRepo.save(user1);
+                        telegramFeign.sendMessageToUser(sendMessage);
+                    }
+                }
+                if(i==0) {
+                    sendMessage.setText("Qabul qilingan savollar yo'q");
                     telegramFeign.sendMessageToUser(sendMessage);
                 }
-            } else {
-                sendMessage.setText("Qabul qilingan savollar yo'q");
-                telegramFeign.sendMessageToUser(sendMessage);
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
@@ -350,10 +365,23 @@ public class UserService {
             User user = getUserFromUpdate(update);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
-            sendMessage.setText("1.Psixologik muammolar soni : " + STATS_FOR_PSYCHOLOGY + "\n" +
-                    "2.Kafedra bilan muammolar soni : " + STATS_FOR_GOVERNMENT + "\n" +
-                    "3.Huquuqlar poymol bo'lishi soni : " + STATS_FOR_RIGHTS + "\n" +
-                    "4.Kontrakt to'lovi yoki turar joy muammosi : " + STATS_FOR_WORK_OR_HOME);
+            List<User> allUsers = userRepo.findAll();
+            int psy=0;
+            int gov=0;
+            int right=0;
+            int workHome=0;
+            if (!allUsers.isEmpty()) {
+                for (User allUser : allUsers) {
+                    psy=psy+allUser.getSTATS_FOR_PSYCHOLOGY();
+                    gov=gov+allUser.getSTATS_FOR_GOVERNMENT();
+                    right=right+allUser.getSTATS_FOR_RIGHTS();
+                    workHome=workHome+allUser.getSTATS_FOR_WORK_OR_HOME();
+                }
+                sendMessage.setText("1.Psixologik muammolar soni : " + psy + "\n" +
+                        "2.Kafedra bilan muammolar soni : " + gov + "\n" +
+                        "3.Huquqlar poymol bo'lishi soni : " + right + "\n" +
+                        "4.Kontrakt to'lovi yoki turar joy muammosi : " + workHome);
+            }
             telegramFeign.sendMessageToUser(sendMessage);
         }catch (Exception e){
             System.out.println(e.getMessage());
@@ -375,10 +403,10 @@ public class UserService {
     }
 
     public void getFile(Update update) {
-        STATS_FOR_WORK_OR_HOME++;
+
         try {
             User user = getUserFromUpdate(update);
-            userForContractOrHome.add(user);
+            user.setSTATS_FOR_WORK_OR_HOME(user.getSTATS_FOR_WORK_OR_HOME()+1);
             if (update.getMessage().hasDocument()) {
                 String fileId = update.getMessage().getDocument().getFileId();
                 user.setFileId(fileId);
@@ -391,6 +419,7 @@ public class UserService {
                 user.setCaption(text);
                 user.setPhoto(true);
             }
+            userRepo.save(user);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(user.getChatId());
             sendMessage.setText("72 soat ichida holatingizni o’rganib chiqqan holda sizga bog’lanamiz ");
@@ -416,28 +445,42 @@ public class UserService {
     public void contractOrHome(Update update) {
         try {
             User user = getUserFromUpdate(update);
-            if (userForContractOrHome.isEmpty()) {
-                SendMessage sendMessage = new SendMessage();
-                sendMessage.setChatId(user.getChatId());
-                sendMessage.setText("Hozircha murojaatlar yo'q");
-                telegramFeign.sendMessageToUser(sendMessage);
-            }
-            for (int i = 0; i < userForContractOrHome.size(); i++) {
-                User userPhoto = userForContractOrHome.poll();
-                if (userPhoto.isPhoto()) {
+            List<User> allUser = userRepo.findAll();
+            int i=0;
+            if (!allUser.isEmpty()) {
+                for (User user1 : allUser) {
+                if (user1.isPhoto()) {
                     SendPhotoOwn sendPhotoOwn = new SendPhotoOwn();
                     sendPhotoOwn.setChatId(user.getChatId());
-                    sendPhotoOwn.setPhoto(userPhoto.getFileId());
-                    sendPhotoOwn.setCaption(userPhoto.getCaption());
-                    telegramFeign.sendPhotoToUser(sendPhotoOwn);
+                    sendPhotoOwn.setPhoto(user1.getFileId());
+                    sendPhotoOwn.setCaption(user1.getCaption());
+                    if(!user1.getFileId().equals(" ")) {
+                        telegramFeign.sendPhotoToUser(sendPhotoOwn);
+                        i++;
+                        user.setFileId(" ");
+                        userRepo.save(user);
+                    }
                 } else {
                     SendDocumentOwn sendDocumentOwn = new SendDocumentOwn();
                     sendDocumentOwn.setChatId(user.getChatId());
-                    sendDocumentOwn.setDocument(userPhoto.getFileId());
-                    sendDocumentOwn.setCaption(userPhoto.getCaption());
-                    telegramFeign.sendDocument(sendDocumentOwn);
+                    sendDocumentOwn.setDocument(user1.getFileId());
+                    sendDocumentOwn.setCaption(user1.getCaption());
+                    if(!user1.getFileId().equals(" ")) {
+                        telegramFeign.sendDocument(sendDocumentOwn);
+                        i++;
+                        user.setFileId(" ");
+                        userRepo.save(user);
+                    }
                 }
             }
+                if(i==0){
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(user.getChatId());
+                    sendMessage.setText("Hozircha qabul qilingan murojaatlar yo'q");
+                    telegramFeign.sendMessageToUser(sendMessage);
+                }
+
+        }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
